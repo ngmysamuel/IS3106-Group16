87e709baba023c65ed49a8b444917a777520ff16
SaiYuchne <yuqiancai987@gmail.com>
2019-03-26 23:27:55 +0800

    finances page for host management

---
+++ ExperienceSystem/ExperienceSystem-war/src/java/jsf/managedbean/HostFinancesManagedBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedbean;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+@Named(value = "hostFinancesManagedBean")
+@ViewScoped
+public class HostFinancesManagedBean implements Serializable {
+
+    private LineChartModel lineModel1;
+     
+    public HostFinancesManagedBean() {
+    }
+    
+    @PostConstruct
+    public void postConstruct() {
+        createLineModel();
+    }
+ 
+    private void createLineModel() {
+        lineModel1 = initLinearModel();
+        lineModel1.setTitle("Linear Chart");
+        lineModel1.setLegendPosition("e");
+        Axis yAxis = lineModel1.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(10);
+    }
+    
+    private LineChartModel initLinearModel() {
+        LineChartModel model = new LineChartModel();
+ 
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+ 
+        series1.set(1, 2);
+        series1.set(2, 1);
+        series1.set(3, 3);
+        series1.set(4, 6);
+        series1.set(5, 8);
+ 
+        model.addSeries(series1);
+ 
+        return model;
+    }
+    
+    public LineChartModel getLineModel1() {
+        return lineModel1;
+    }
+}
